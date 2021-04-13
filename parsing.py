83b4e3d40f627bdc4be485298a8972a05a2d3a49
Никита <64539102+ruk228@users.noreply.github.com>
2021-04-13 19:51:06 +0400

    UPDATE

--- parsing.py
+++ parsing.py
@@ -54,14 +54,10 @@ def parse_book_page(id_book):
     url = 'https://tululu.org/b{}/'.format(id_book)
     response = get_response(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    filename = soup.find('table', class_='tabs')\
-    .find('td', class_='ow_px_td').find('h1').text
-    image_name = soup.find('table', class_='tabs')\
-    .find('td', class_='ow_px_td').find('table').find('img')['src']
-    comments = soup.find('table', class_='tabs')\
-    .find('div', class_='texts').find_all('span', class_='black')[entrance].text
-    genre = soup.find('table', class_='tabs')\
-    .find('span', class_='d_book').find_all('a')[entrance].text
+    filename = soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('h1').text
+    image_name = soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('table').find('img')['src']
+    comments = soup.find('table', class_='tabs').find('div', class_='texts').find_all('span', class_='black')[entrance].text
+    genre = soup.find('table', class_='tabs').find('span', class_='d_book').find_all('a')[entrance].text
     book_page_information = {
         'filename': filename,
         'name_book': filename.split('::')[entrance],
@@ -80,15 +76,7 @@ def get_link_the_text_book(id_book):
     return response
 
 
-def get_the_output_of_information_about_the_book(book_page_information):
-    print(book_page_information['name_book'])
-    print(book_page_information['author'])
-    print(book_page_information['comments'])
-    print(book_page_information['genre'])
-    print()
-
-
-def get_start_id_and_end_id():
+def get_a_numeric_argument():
     parser = argparse.ArgumentParser(description='Скачивание книг\картинки с сайта')
     parser.add_argument('start_id', help='id книг от', type=int)
     parser.add_argument('end_id', help='id книг до', type=int)
@@ -97,15 +85,13 @@ def get_start_id_and_end_id():
 
 
 if __name__ == '__main__':
-    args = get_start_id_and_end_id()
+    args = get_a_numeric_argument()
     urllib3.disable_warnings()
-    numbers = range(args.start_id, args.end_id)
 
-    for id_book in numbers:
+    for id_book in range(args.start_id, args.end_id):
         try:
             response = get_link_the_text_book(id_book)
             book_page_information = parse_book_page(id_book)
             download_txt(response, book_page_information, book_page_information)
-            get_the_output_of_information_about_the_book(book_page_information)
-        except:
+        finally:
             pass
