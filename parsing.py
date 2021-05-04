231a2f97854ca1ca7bfaa9bc6e9ed6fed37b5d37
Никита <64539102+ruk228@users.noreply.github.com>
2021-05-04 22:00:07 +0400

    Bug fixes
    name changes
    Fixed typos
    the function is displayed in name == main

--- parsing.py
+++ parsing.py
@@ -2,12 +2,13 @@ import requests
 import os
 import urllib3
 import argparse
+import logging
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 
 
-def download_image(image_name, folder = 'img/'):
-    filename = image_name['image_name']
+def download_image(book_page_information, folder = 'img/'):
+    filename = book_page_information['image_name']
     url = 'https://tululu.org/{}'.format(filename)
     response = get_response(url)
     filename = filename.split("/")[2]
@@ -18,20 +19,18 @@ def download_image(image_name, folder = 'img/'):
 
 
 def check_for_redirect(response):
-    status_code = response.history
+    history = response.history
 
-    if status_code:
-        raise requests.HTTPError(status_code)
+    if history:
+        raise requests.HTTPError(history)
 
 
-def download_txt(response, book_page_information, image_name, folder='books/'):
-    catalog_books = os.path.join('{}', '{}.txt').format(sanitize_filename(folder)
-    , sanitize_filename(book_page_information['filename']))
+def download_txt(response, book_page_information, folder='books/'):
+    catalog_books = os.path.join('{}', '{}.txt').format(
+    sanitize_filename(folder), sanitize_filename(book_page_information['filename']))
 
-    with open(catalog_books, 'w') as file:
+    with open(catalog_books, 'w', encoding='utf-8') as file:
         file.write(response.text)
-
-    download_image(image_name)
 
 
 def get_response(url):
@@ -41,19 +40,18 @@ def get_response(url):
     return response
 
 
-def parse_book_page(id_book):
-    url = 'https://tululu.org/b{}/'.format(id_book)
+def parse_book_page(book_id):
+    url = 'https://tululu.org/b{}/'.format(book_id)
     response = get_response(url)
     soup = BeautifulSoup(response.text, "html.parser")
     filename = soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('h1').text
     image_name = soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('table').find('img')['src']
     all_comments = soup.find('table', class_='tabs').find('div', class_='texts')
+    comments = None
     if all_comments:
         comments = all_comments.find_all('span', class_='black')[0].text
-    else:
-        comments = None
 
-    genre = soup.find('table', class_='tabs').find('span', class_='d_book').find_all('a')[0].text
+    genre = soup.find('table', class_='tabs').find('span', class_='d_book').find('a').text
     book_page_information = {
         'filename': filename,
         'name_book': filename.split('::')[0],
@@ -65,15 +63,16 @@ def parse_book_page(id_book):
     return book_page_information
 
 
-def get_link_the_book(id_book):
-    get_parameter = '?id={}'.format(id_book)
-    url = 'https://tululu.org/txt.php{}'.format(get_parameter)
+def get_link_book(book_id):
+    payload = {'id': '{}'.format(book_id)}
+    response = requests.get('https://tululu.org/txt.php', params=payload, verify=False)
+    url = response.url
     response = get_response(url)
     check_for_redirect(response)
     return response
 
 
-def get_a_numeric_argument():
+def get_args():
     parser = argparse.ArgumentParser(description='Скачивание книг\картинки с сайта')
     parser.add_argument('start_id', help='id книг от', type=int)
     parser.add_argument('end_id', help='id книг до', type=int)
@@ -82,13 +81,16 @@ def get_a_numeric_argument():
 
 
 if __name__ == '__main__':
-    args = get_a_numeric_argument()
+    args = get_args()
+    logging.basicConfig(level = logging.ERROR)
     urllib3.disable_warnings()
 
-    for id_book in range(args.start_id, args.end_id):
+    for book_id in range(args.start_id, args.end_id):
         try:
-            response = get_link_the_book(id_book) 
-            book_page_information = parse_book_page(id_book)
-            download_txt(response, book_page_information, book_page_information)
+            response = get_link_book(book_id) 
+            book_page_information = parse_book_page(book_id)
+            download_txt(response, book_page_information)
+            download_image(book_page_information)
         except requests.HTTPError:
+            logging.error('Такого id нет на сайте')
             continue 
